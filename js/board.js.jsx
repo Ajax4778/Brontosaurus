3103e67d7bf6528dc2e15aafffcc152cec6541aa
Adithya Manohar <adi4778@gmail.com>
2015-11-07 18:39:16 -0800

    set up make move

--- js/board.js.jsx
+++ js/board.js.jsx
@@ -1,27 +1,59 @@
 var Board = React.createClass({
   EMPTY_BOARD: [],
-  
+
+  _grid: EMPTY_BOARD,
+
   getInitialState: function() {
-    return { grid: EMPTY_BOARD, selected: [] };
+    return { grid: _grid, selected: [] };
   },
 
-  _onChangeEvent: function() {
-    //this.setState({ selected: selectionChange })
+  onChangeEvent: function() {
+    this.setState({ grid: _grid });
   },
 
-  _buildTiles: function() {
+  makeMove: function(dir) {
+    var newGrid;
+    switch (dir) {
+      case 'left':
+        // move board left
+        // set newGrid
+        break;
+      case 'right':
+        // move board right
+        // set newGrid
+        break;
+      case 'up':
+        //move board up
+        // set newGrid
+        break;
+      case 'down':
+        //move board down
+        // set newGrid
+        break;
+    }
+    _grid = newGrid;
+    this.onChangeEvent();
+  },
+
+  buildTiles: function() {
     var tiles = this.state.grid.map(function(tile) {
       <div className='tile'>{tile.value}</div>
     });
     return tiles;
   },
 
+  handleKeyDown: function(e) {
+    e.preventDefault();
+    var dir = e.keyInput; ////////
+    this.makeMove(dir);
+  },
+
   render: function() {
-    var tiles = this._buildTiles()
+    var tiles = this.buildTiles();
     return (
-      <ul className='gameBoard'>
+      <ul onKeyDown={this.handleKeyDown} className='gameBoard'>
         {tiles}
       </ul>
-    )
+    );
   }
 })
