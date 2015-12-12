eacb3edb98ca5d189f2e6d49c22d6a7e738d4627
Adithya Manohar <adi4778@gmail.com>
2015-12-11 23:21:13 -0800

    set grid to 25

--- javascripts/board.jsx
+++ javascripts/board.jsx
@@ -2,79 +2,94 @@ var React = require('react');
 var Tile = require('./tile.jsx');
 
 var Board = React.createClass({
-  _grid: [],
+  _gridSize: 26,
 
   getInitialState: function() {
-    return { grid: this._grid, selected: [] };
+    var blankGrid = new Array(this._gridSize).join('...').split('');
+    return {
+      grid: blankGrid,
+      nullTiles: blankGrid.map(function(_, i) { return i; }),
+      selected: []
+    };
   },
 
   componentDidMount: function() {
     document.addEventListener('keypress', this.handleKeyPress);
-    this._grid = window.TileStore.startNewGame();
-    this.onChangeEvent();
+    this.startGame();
   },
 
   componentWillUnmount: function() {
     document.removeEventListener('keypress', this.handleKeyPress);
   },
 
-  onChangeEvent: function() {
-    this.setState({ grid: this._grid });
-  },
+  // onChangeEvent: function() {
+  //   this.setState({ grid: this._grid });
+  // },
 
-  makeMove: function(dir) {
-    var newGrid;
-    switch (dir) {
-      case 'left':
-        // move board left
-        // set newGrid
-        break;
-      case 'right':
-        // move board right
-        // set newGrid
-        break;
-      case 'up':
-        //move board up
-        // set newGrid
-        break;
-      case 'down':
-        //move board down
-        // set newGrid
-        break;
-    }
-    this._grid = newGrid;
-    this.onChangeEvent();
-  },
+  // makeMove: function(dir) {
+  //   var newGrid;
+  //   switch (dir) {
+  //     case 'left':
+  //       // move board left
+  //       // set newGrid
+  //       break;
+  //     case 'right':
+  //       // move board right
+  //       // set newGrid
+  //       break;
+  //     case 'up':
+  //       //move board up
+  //       // set newGrid
+  //       break;
+  //     case 'down':
+  //       //move board down
+  //       // set newGrid
+  //       break;
+  //   }
+  //   this._grid = newGrid;
+  //   this.onChangeEvent();
+  // },
 
-  buildTiles: function() {
-    var id = 0;
-    var tiles = this.state.grid.map(function(letter) {
-      id += 1;
-      return <Tile key={id} letter={letter}/>;
+  startGame: function() {
+    var newTiles = TileStore.startNewGame();
+    var nullTiles = this.state.nullTiles;
+    var grid = this.state.grid;
+    newTiles.forEach(function(tile) {
+      newTileIdx = parseInt(Math.random * nullTiles.length);
+      grid[newTileIdx] = tile;
+      nullTiles.splice(newTileIdx, 1);
     });
-    while (tiles.length < 25) {
-      id += 1;
-      tiles.push(<Tile key={id} letter='0'/>);
-    }
-    return tiles;
+    this.setState({ grid: grid, nullTiles: nullTiles });
+  },
+
+  addTiles: function() {
+    var newTiles = TileStore.addTiles();
+    var nullTiles = this.state.nullTiles;
+    var grid = this.state.grid;
+    newTiles.forEach(function(tile) {
+      if (nullTiles.length === 0) {
+        this.setState({ grid: grid, nullTiles: nullTiles });
+      } else {
+        newTileIdx = parseInt(Math.random * nullTiles.length);
+        grid[newTileIdx] = tile;
+        nullTiles.splice(newTileIdx, 1);
+      }
+    });
+    this.setState({ grid: grid, nullTiles: nullTiles });
   },
 
   handleKeyPress: function(e) {
     e.preventDefault();
     console.log(e.keyCode);
-    var newTiles = TileStore.addTiles();
-    this._grid = this._grid.concat(newTiles);
-    this._grid.splice(25);
-    this.onChangeEvent();
+    this.addTiles();
     // var dir = e.keyInput; ////////
     // this.makeMove(dir);
   },
 
   render: function() {
-    var tiles = this.buildTiles();
     return (
       <ul className='board container'>
-        {tiles}
+        {this.state.grid}
       </ul>
     );
   }
